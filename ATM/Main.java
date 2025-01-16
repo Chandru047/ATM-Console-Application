997861b9b749f102233135a1e48f14758bfffdfe
Chandru <107676929+Chandru047@users.noreply.github.com>
2025-01-16 20:57:19 +0530

    Update Main.java

--- ATM/Main.java
+++ ATM/Main.java
@@ -2,7 +2,6 @@ class Main
 {
     public static void main(String[] args) throws CloneNotSupportedException
     {
-        ATMActions atm = new ATMActions(); // Create a object for AtmActions
-        atm.start(); // call the start method  in atm
+        ATMActions.start(); // call the start method  in atm
     }
 }
